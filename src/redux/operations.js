1005762c7a3131297d3b08685ddba82cf5490ab4
MaliyCo <115729512+oleksandrmaliy@users.noreply.github.com>
2025-03-27 23:14:28 +0200

    commit operations

--- src/redux/operations.js
+++ src/redux/operations.js
@@ -1,9 +1,9 @@
-import axios from 'axios';
+// import axios from 'axios';
 
-axios.defaults.baseURL = 'https://dummyjson.com/products';
+// axios.defaults.baseURL = 'https://dummyjson.com/products';
 
-export const fetchData = async () => {
-  const response = await axios.get(`/`);
-  console.log(response.data.products);
-  return response.data.products;
-};
+// export const fetchData = async () => {
+//   const response = await axios.get(`/`);
+//   console.log(response.data.products);
+//   return response.data.products;
+// };
