cf2a1e4081de4c0b58be4b4f35ec5b0122cff7b3
MaliyCo <115729512+oleksandrmaliy@users.noreply.github.com>
2025-04-01 23:10:18 +0300

    clean comments

--- src/components/api.js
+++ src/components/api.js
@@ -3,16 +3,6 @@ import axios from 'axios';
 // axios.defaults.baseURL = 'https://dummyjson.com/test';
 axios.defaults.baseURL = 'https://dummyjson.com/products';
 
-// {
-//     "albumId": 1,
-//     "id": 1,
-//     "title": "accusamus beatae ad facilis cum similique qui sunt",
-//     "url": "https://via.placeholder.com/600/92c952",
-//     "thumbnailUrl": "https://via.placeholder.com/150/92c952"
-//     },
-
-// https://jsonplaceholder.typicode.com/albums/1/photos
-
 export const fetchData = async () => {
   const response = await axios.get(`/`);
   console.log(response.data.products);
