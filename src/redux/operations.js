fac1ed4e1ea85f958bb6d3067a499ea2cce51eda
MaliyCo <115729512+oleksandrmaliy@users.noreply.github.com>
2025-04-02 21:58:07 +0300

    add operations

---
+++ src/redux/operations.js
@@ -0,0 +1,10 @@
+// import axios from 'axios';
+
+// axios.defaults.baseURL = 'https://dummyjson.com/test';
+// axios.defaults.baseURL = 'https://dummyjson.com/products';
+
+// export const fetchData = async () => {
+//   const response = await axios.get(`/`);
+//   console.log(response.data.products);
+//   return response.data.products;
+// };
