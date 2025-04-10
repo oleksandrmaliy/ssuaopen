8a30950c0bd83cc29b51735bee47053829aab52d
MaliyCo <115729512+oleksandrmaliy@users.noreply.github.com>
2025-04-10 23:20:08 +0300

    change api

--- src/components/api.js
+++ src/components/api.js
@@ -8,14 +8,3 @@
 //   console.log(response.data.products);
 //   return response.data.products;
 // };
-
-// import axios from 'axios';
-
-// // axios.defaults.baseURL = 'https://dummyjson.com/test';
-// axios.defaults.baseURL = 'https://dummyjson.com/products';
-
-// export const fetchData = async () => {
-//   const response = await axios.get(`/`);
-//   console.log(response.data.products);
-//   return response.data.products;
-// };
