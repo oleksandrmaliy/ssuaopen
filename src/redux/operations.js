849c4286fa4b785c6af73e824f3e8587646cca0e
MaliyCo <115729512+oleksandrmaliy@users.noreply.github.com>
2025-04-13 23:10:34 +0300

    add fetchSurfers

--- src/redux/operations.js
+++ src/redux/operations.js
@@ -1,10 +1,16 @@
 import axios from 'axios';
+import { fetchInProgress, fetchSuccess, fetchError } from './surfersSlice';
 
 // axios.defaults.baseURL = 'https://dummyjson.com/test';
 axios.defaults.baseURL = 'https://66b388ab7fba54a5b7ed51b4.mockapi.io/';
 
-export const fetchSurfers = async () => {
-  const response = await axios.get(`/surfers`);
-  console.log(response.data);
-  return response.data;
+export const fetchSurfers = () => async (dispatch) => {
+  try {
+    dispatch(fetchInProgress());
+    const response = await axios.get(`/surfers`);
+    console.log(response.data);
+    dispatch(fetchSuccess(response.data));
+  } catch (error) {
+    dispatch(fetchError(error.message));
+  }
 };
---
+++ src/redux/surfersSlice.js
@@ -0,0 +1,28 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const surfersSlice = createSlice({
+  name: 'surfers',
+  initialState: {
+    players: [],
+    isLoading: false,
+    error: null,
+  },
+  reducers: {
+    fetchInProgress(state) {
+      state.isLoading = true;
+    },
+    fetchSuccess(state, action) {
+      state.isLoading = false;
+      state.error = null;
+      state.players = action.payload;
+    },
+    fetchError(state, action) {
+      state.isLoading = false;
+      state.error = action.payload;
+    },
+  },
+});
+
+export const { fetchInProgress, fetchSuccess, fetchError } =
+  surfersSlice.actions;
+export const selectSurfers = (state) => state.surfers.players;
