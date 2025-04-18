70b1e3e0b58deefe17f74c48035c940414c94ade
MaliyCo <115729512+oleksandrmaliy@users.noreply.github.com>
2025-04-18 23:11:30 +0300

    receive data

--- src/pages/first.jsx
+++ src/pages/first.jsx
@@ -2,13 +2,13 @@
 // import { fetchData } from '../components/api.js';
 import Title from '../components/title.jsx';
 
-// import Surfers from '../components/surfers.jsx';
+import Surfers from '../components/surfers.jsx';
 
 const First = () => {
   return (
     <>
       <Title>First page</Title>
-      {/* <Surfers /> */}
+      <Surfers />
     </>
   );
 };
