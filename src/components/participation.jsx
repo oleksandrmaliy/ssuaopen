ef1424584951158cf388fe55124a6af0fa801a25
MaliyCo <115729512+oleksandrmaliy@users.noreply.github.com>
2025-04-26 15:42:23 +0300

    refix participation form

--- src/components/participation.jsx
+++ src/components/participation.jsx
@@ -1,64 +1,65 @@
-import { useId, useRef } from 'react';
+import { useId } from 'react';
 import { Formik, Form, Field, ErrorMessage } from 'formik';
-import * as Yup from 'yup';
+// import * as Yup from 'yup';
 
-const MAX_VIDEO_SIZE = 52428800; // 50MB
-const SUPPORTED_VIDEO_FORMATS = [
-  'video/mp4',
-  'video/avi',
-  'video/mkv',
-  'video/quicktime',
-];
+// const MAX_VIDEO_SIZE = 52428800; // 50MB
+// const SUPPORTED_VIDEO_FORMATS = [
+//   'video/mp4',
+//   'video/avi',
+//   'video/mkv',
+//   'video/quicktime',
+// ];
 
 const userValues = {
+  name: '',
+  avatar: null,
+  score: '',
   week: '',
-  video: null,
-  text: '',
+  date: '',
+  file: null,
 };
 
-const userSchema = Yup.object().shape({
-  week: Yup.string().required('Оберіть тиждень'),
-  video: Yup.mixed()
-    .required('Відеофайл обов’язковий')
-    .test('is-valid-size', 'Файл завеликий (макс. 50MB)', (value) => {
-      return value && value.size <= MAX_VIDEO_SIZE;
-    })
-    .test(
-      'fileType',
-      'Непідтримуваний формат (дозволено MP4, AVI, MKV, MOV)',
-      (value) => {
-        return value && SUPPORTED_VIDEO_FORMATS.includes(value.type);
-      },
-    ),
-  text: Yup.string()
-    .min(6, 'Короткий!')
-    .max(500, 'Довгий!')
-    .required("Обов'язкове поле"),
-});
-
-const ErrorToast = ({ children }) => {
-  return <h3 style={{ color: 'red' }}>{children}</h3>;
-};
+// const userSchema = Yup.object().shape({
+//   week: Yup.string().required('Оберіть тиждень'),
+//   video: Yup.mixed()
+//     .required('Відеофайл обов’язковий')
+//     .test('is-valid-size', 'Файл завеликий (макс. 50MB)', (value) => {
+//       return value && value.size <= MAX_VIDEO_SIZE;
+//     })
+//     .test(
+//       'fileType',
+//       'Непідтримуваний формат (дозволено MP4, AVI, MKV, MOV)',
+//       (value) => {
+//         return value && SUPPORTED_VIDEO_FORMATS.includes(value.type);
+//       },
+//     ),
+//   text: Yup.string()
+//     .min(6, 'Короткий!')
+//     .max(500, 'Довгий!')
+//     .required("Обов'язкове поле"),
+// });
 
 const Participation = ({ className }) => {
+  const nameId = useId();
+  const avatarId = useId();
+  const scoreId = useId();
   const weekId = useId();
+  const dateId = useId();
   const fileId = useId();
-  const textAreaId = useId();
 
- 
-  const videoInputRef = useRef(null); // Створення рефу для input[type="file"]
+  // const videoInputRef = useRef(null); // Створення рефу для input[type="file"]
 
   const handleSubmit = (values, actions) => {
     console.log('Форма відправлена:', values);
-    console.log(values.video);
-    console.log(values.video.size);
-    console.log(values.video.type);
+    // console.log(values.video);
+    // console.log(values.video.size);
+    // console.log(values.video.type);
 
     actions.resetForm();
 
-    if (videoInputRef.current) {
-      videoInputRef.current.value = ''; // очищаємо значення файлу
-    }
+    // if (videoInputRef.current) {
+    //   videoInputRef.current.value = ''; // очищаємо значення файлу
+    // }
   };
 
   return (
@@ -67,55 +68,67 @@ const Participation = ({ className }) => {
       <Formik
         initialValues={userValues}
         onSubmit={handleSubmit}
-        validationSchema={userSchema}
+        // validationSchema={userSchema}
       >
-        {({ setFieldValue }) => (
-          <Form>
-            <div>
-              <label htmlFor={weekId}>Тиждень</label>
-              <Field
-                type="week"
-                name="week"
-                id={weekId}
-                placeholder="Тиждень"
-              />
-              <ErrorMessage name="week" component={ErrorToast} />
-            </div>
-         
+        <Form>
+          <div>
+            <label htmlFor={nameId}>Ім&apos;я</label>
+            <Field type="string" name="name" id={nameId} placeholder="Name" />
+            <ErrorMessage name="name" />
+          </div>
 
-            <div>
-              <label htmlFor={fileId}>Відеофайл</label>
-              <input
-                ref={videoInputRef} // Прив'язуємо реф
-                id={fileId}
-                name="video"
-                type="file"
-                accept="video/*"
-                onChange={(event) => {
-                  const file = event.currentTarget.files?.[0];
-                  setFieldValue('video', file);
-                }}
-              />
-              <ErrorMessage name="video" component={ErrorToast} />
-            </div>
+          <div>
+            <label htmlFor={avatarId}>Аватар</label>
+            <Field
+              type="file"
+              name="avatar"
+              id={avatarId}
+              placeholder="Тиждень"
+            />
+            <ErrorMessage name="avatar" />
+          </div>
 
-            <div>
-              <label htmlFor={textAreaId}>Про себе</label>
-              <Field
-                as="textarea"
-                rows="5"
-                cols="50"
-                wrap="hard"
-                name="text"
-                id={textAreaId}
-                placeholder="Про себе"
-              />
-              <ErrorMessage name="text" component={ErrorToast} />
-            </div>
+          <div>
+            <label htmlFor={scoreId}>Рахунок</label>
+            <Field
+              type="number"
+              name="score"
+              id={scoreId}
+              placeholder="Score"
+            />
+            <ErrorMessage name="score" />
+          </div>
 
-            <button type="submit">Надіслати</button>
-          </Form>
-        )}
+          <div>
+            <label htmlFor={weekId}>Тиждень</label>
+            <Field
+              type="week"
+              name="week"
+              id={weekId}
+              // placeholder="Week number"
+            />
+            <ErrorMessage name="week" />
+          </div>
+
+          <div>
+            <label htmlFor={dateId}>Дата участі</label>
+            <Field
+              type="date"
+              name="date"
+              id={dateId}
+              // placeholder="dd-mm-yyyy"
+            />
+            <ErrorMessage name="date" />
+          </div>
+
+          <div>
+            <label htmlFor={fileId}>Файл</label>
+            <Field type="file" name="file" id={fileId} placeholder="File" />
+            <ErrorMessage name="file" />
+          </div>
+
+          <button type="submit">Надіслати</button>
+        </Form>
       </Formik>
     </div>
   );
