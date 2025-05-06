45526b4481ab0448a21bea396df6c37ad7036827
MaliyCo <115729512+oleksandrmaliy@users.noreply.github.com>
2025-05-06 16:23:28 +0300

    edit operations

--- src/redux/operations.js
+++ src/redux/operations.js
@@ -1,20 +1,4 @@
 import axios from 'axios';
-// import { fetchInProgress, fetchSuccess, fetchError } from './surfersSlice';
-
-// axios.defaults.baseURL = 'https://dummyjson.com/test';
-// axios.defaults.baseURL = 'https://66b388ab7fba54a5b7ed51b4.mockapi.io/';
-
-// export const fetchSurfers = () => async (dispatch) => {
-//   try {
-//     dispatch(fetchInProgress());
-//     const response = await axios.get(`/surfers`);
-//     // console.log(response.data);
-//     dispatch(fetchSuccess(response.data));
-//   } catch (error) {
-//     dispatch(fetchError(error.message));
-//   }
-// };
-
 import { createAsyncThunk } from '@reduxjs/toolkit';
 
 axios.defaults.baseURL = 'https://66b388ab7fba54a5b7ed51b4.mockapi.io/';
@@ -25,29 +9,6 @@ export const fetchSurfers = createAsyncThunk(
   async (_, thunkAPI) => {
     try {
       const response = await axios.get('/surfers');
-      // При успішному запиті повертаємо проміс із даними
-      return response.data;
-    } catch (error) {
-      // При помилці запиту повертаємо проміс
-      // який буде відхилений з текстом помилки
-      return thunkAPI.rejectWithValue(error.message);
-    }
-  },
-
-  //   async () => {
-  //   const response = await axios.get('/surfers');
-  //   return response.data;
-  // }
-);
-
-// export default fetchSurfers;
-
-export const deleteSurfer = createAsyncThunk(
-  'surfers/deleteSurfer',
-  async (playerId, thunkAPI) => {
-    try {
-      const response = await axios.delete(`/surfers/${playerId}`);
-      console.log(playerId);
       return response.data;
     } catch (error) {
       return thunkAPI.rejectWithValue(error.message);
@@ -61,6 +22,19 @@ export const postSurfer = createAsyncThunk(
   async (player, thunkAPI) => {
     try {
       const response = await axios.post('/surfers', player);
+      return response.data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message);
+    }
+  },
+);
+
+export const deleteSurfer = createAsyncThunk(
+  'surfers/deleteSurfer',
+
+  async (playerId, thunkAPI) => {
+    try {
+      const response = await axios.delete(`/surfers/${playerId}`);
       return response.data;
     } catch (error) {
       return thunkAPI.rejectWithValue(error.message);
