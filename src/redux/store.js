fea734fd8958fc8dc512d5bae644f2d34a75d831
MaliyCo <115729512+oleksandrmaliy@users.noreply.github.com>
2025-05-08 22:07:54 +0300

    clear store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,13 +1,9 @@
 import { configureStore } from '@reduxjs/toolkit';
 
-// import firstReducer from './firstSlice';
-// import secondReducer from './secondSlice';
 import surfersReducer from './surfersSlice';
 
 export const store = configureStore({
   reducer: {
-    // first: firstReducer,
-    // second: secondReducer,
     surfers: surfersReducer,
   },
 });
