3d08b2c3fdd732f4a78c2bee505d60aa194a3b35
MaliyCo <115729512+oleksandrmaliy@users.noreply.github.com>
2025-05-19 22:58:17 +0300

    change title

--- src/components/title.jsx
+++ src/components/title.jsx
@@ -1,6 +1,6 @@
 const Title = ({ children }) => {
   return (
-    <div className="mb-10">
+    <div className="mb-12">
       <h3 className="rounded-lg bg-green-500 p-4 text-center text-2xl font-bold">
         {children}
       </h3>
